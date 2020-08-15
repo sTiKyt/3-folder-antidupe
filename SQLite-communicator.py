72fe8bb4fd2bbe3455d2269d3025ab869629a185
TheK0tYaRa <alod123@mail.ua>
2020-08-15 21:28:45 +0300

    all_files done

--- SQLite-communicator.py
+++ SQLite-communicator.py
@@ -5,6 +5,7 @@ from os import path
 import sqlite3
 
 #variables
+ALL = ''
 DB_NAME = 'sha256.sql'
 FIRST_LAUNCH = False
 FOLDER = '../mixed/'
@@ -14,6 +15,13 @@ FILES_SORTED = [[],[],[]]
 HASH_SORTED = []
 
 #functions
+def all_files(FILES_SORTED):
+    NUMBER = 0
+    for l1 in range(len(FILES_SORTED)):
+        for _ in range(len(FILES_SORTED[l1])):
+            NUMBER += 1
+    return NUMBER
+
 def any_to_sha256(in1):
     output = ''
     with open(in1, "rb") as f:
@@ -71,30 +79,21 @@ def folder_to_list(FOLDER):
     FILES_SORTED[2] = listdir(FOLDER+'videos/')
     return FILES_SORTED
 
-def list_to_sha256(FILES_SORTED,FOLDER):#incomplete
+def list_to_sha256(FILES_SORTED,FOLDER,ALL):#incomplete
     HASH_SORTED = [[],[],[]]
     PATHS_SORTED = [[],[],[]]
+    PROGRESS = 0
     for l1 in range(len(FILES_SORTED[0])):
         PATHS_SORTED[0].append(FOLDER+'files/'+FILES_SORTED[0][l1])
     for l1 in range(len(FILES_SORTED[1])):
         PATHS_SORTED[1].append(FOLDER+'photos/'+FILES_SORTED[1][l1])
     for l1 in range(len(FILES_SORTED[2])):
         PATHS_SORTED[2].append(FOLDER+'videos/'+FILES_SORTED[2][l1])
-    
     for l1 in range(len(PATHS_SORTED)):
         for l2 in range(len(PATHS_SORTED[l1])):
-            tmp1 = PATHS_SORTED[l1][l2]
-            HASH_SORTED[l1].append(any_to_sha256(tmp1))
-#            with open(PATHS_SORTED[l1][l2], 'rb') as file:
-#                TEMP = str(file.read())
-#                HASH_SORTED[l1].append(sha256(file.read()).hexdigest())#I/O crime
-                
-            
-#    print(HASH_SORTED[0][0])#temporary
-    
-#    for l1 in range(len(PATHS_SORTED)):
-#        for l2 in range(len(PATHS_SORTED[l1])):
-#            HASH_SORTED[l1].append(sha256(PATHS_SORTED[l1][l2].encode('utf-8')).hexdigest())
+            HASH_SORTED[l1].append(any_to_sha256(PATHS_SORTED[l1][l2]))
+            PROGRESS += 1
+            print("\rProgress: " + PROGRESS + " of " + ALL)
     return HASH_SORTED
 
 def load_db_to_list(): #TODO make
@@ -136,7 +135,8 @@ if not path.isfile(DB_NAME):#If the database doesn't exists
     FIRST_LAUNCH = create_db()#We create it and note that down
 
 FILES_SORTED = folder_to_list(FOLDER)#Then the folder files get into the file list
-HASH_SORTED = list_to_sha256(FILES_SORTED,FOLDER)#Then the file list gets converted to a sha256 list
+ALL = all_files(FILES_SORTED)#Then count all the files to display progress
+HASH_SORTED = list_to_sha256(FILES_SORTED,FOLDER,ALL)#Then the file list gets converted to a sha256 list
 #HASH_SORTED = clean_sha256(HASH_SORTED)#Then the sha256 list gets cleaned of duplicates
 #dupes_to_dupe_list()#Then the duplicated files get into a dupe list
 #move_dirty()#Then duplicates are being moved to a DUPES folder
