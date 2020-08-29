aebf94727e37babc82a38859f37d0a011acf8483
TheK0tYaRa <alod123@mail.ua>
2020-08-29 12:28:06 +0300

    i now hate sqlite

--- SQLite-communicator.py
+++ SQLite-communicator.py
@@ -1,11 +1,9 @@
 from hashlib import sha256
 from os import listdir
 from os import path
-import sqlite3
 
 #variables
 total = ''
-db_name = 'sha256.sql'
 debug = True#set this to False if you want everything to work properly
 first_launch = False
 folder = '../mixed/'
@@ -44,19 +42,19 @@ def clean_sha256(hash_sorted):#untested
     #print(str(HASH_DUPES))#temporary
     return hash_sorted_clean
 
-def clean_sha256_with_db():#incomplete
+def clean_sha256_with_base():#incomplete
     void()
 
-def create_db():
-    first_launch = True
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('CREATE TABLE files(sha256 text)')
-    c.execute('CREATE TABLE images(sha256 text)')
-    c.execute('CREATE TABLE videos(sha256 text)')
-    conn.commit()
-    conn.close()
-    return first_launch
+#def create_db():
+#    first_launch = True
+#    conn = sqlite3.connect(base_name)
+#    c = conn.cursor()
+#    c.execute('CREATE TABLE files(sha256 text)')
+#    c.execute('CREATE TABLE images(sha256 text)')
+#    c.execute('CREATE TABLE videos(sha256 text)')
+#    conn.commit()
+#    conn.close()
+#    return first_launch
 
 def folder_to_list(folder):
     files_sorted[0] = listdir(folder+'files/')
@@ -82,36 +80,36 @@ def list_to_sha256(files_sorted,folder,total):
     print('')
     return hash_sorted
 
-def load_db_to_list(db_name): #TODO make
-    db_list = [[],[],[]]
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('')
-    print(c.fetchall())
-    return db_list
+def load_base_to_list(base_name): #TODO make
+    base_list = [[],[],[]]
+    #conn = sqlite3.connect(base_name)
+    #c = conn.cursor()
+    #c.execute('')
+    #print(c.fetchall())
+    return base_list
 
-def merge_sha256_with_db(total):
+def merge_sha256_with_base(total):
     progress = 0
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
+    #conn = sqlite3.connect(base_name)
+    #c = conn.cursor()
     for l1 in range(len(hash_sorted[0])):
         params = hash_sorted[0][l1]
-        c.execute('INSERT INTO files VALUES(?)',(params,))
+        #c.execute('INSERT INTO files VALUES(?)',(params,))
         progress += 1
         print("Stored: " + str(progress) + " of " + str(total), end="\r")
     for l1 in range(len(hash_sorted[1])):
         params = hash_sorted[1][l1]
-        c.execute('INSERT INTO images VALUES(?)',(params,))
+        #c.execute('INSERT INTO images VALUES(?)',(params,))
         progress += 1
         print("Stored: " + str(progress) + " of " + str(total), end="\r")
     for l1 in range(len(hash_sorted[2])):
         params = hash_sorted[2][l1]
-        c.execute('INSERT INTO videos VALUES(?)',(params,))
+        #c.execute('INSERT INTO videos VALUES(?)',(params,))
         progress += 1
         print("Stored: " + str(progress) + " of " + str(total), end="\r")
     print('')
-    conn.commit()
-    conn.close()
+    #conn.commit()
+    #conn.close()
 
 def move_clean():#incomplete
     void()
@@ -127,8 +125,8 @@ def void():
     return
 
 #main
-if not path.isfile(db_name):#If the database doesn't exists
-    first_launch = create_db()#We create it and note that down
+#if not path.isfile(base_name):#If the database doesn't exists
+#    first_launch = #create_db()#We create it and note that down
 
 files_sorted = folder_to_list(folder)#Then the folder files get into the file list
 if debug:
@@ -139,11 +137,11 @@ total = tots(files_sorted)#Then count all the files to display progress
 hash_sorted = list_to_sha256(files_sorted,folder,total)#Then the file list gets converted to a sha256 list 
 hash_sorted = clean_sha256(hash_sorted)#Then the sha256 list gets cleaned of duplicates
 if not first_launch:
-    db_list = load_db_to_list(db_name)#Then database lists get loaded in
-    ##clean_sha256_with_db()#Then database lists get compared to each of the representing groups of sha256 lists
+    #base_list = load_base_to_list(base_name)#Then database lists get loaded in
+    ##clean_sha256_with_base()#Then database lists get compared to each of the representing groups of sha256 lists
     void()
 total = tots(hash_sorted)
-merge_sha256_with_db(total)#Then the cleaned sha256 list gets merged into the database
+merge_sha256_with_base(total)#Then the cleaned sha256 list gets merged into the database
 ##move_dirty()#Then the last duplicate files get moved to DUPES folder
 ##move_clean()#Then remaining files get moved to the UPLOAD folder
 ###stats()#Then the statistics get displayed
